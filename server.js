ed946467295a4cac529bf3325f153cf5eeb62882
Ryan Baumann <rsbaumann@gmail.com>
2017-01-23 20:28:25 -0800

    initial commit

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 var express = require("express"),
     app = express();
+var mapboxgl = require('mapbox-gl');
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
-
-app.get("/hello", function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"})
-    response.end("Hello World!\n");
+//Get Mapbox Credentials
+var config = JSON.parse(process.env.VCAP_SERVICES);
+var mapboxAccessToken;
+config['user-provided'].forEach(function(service) {
+    if (service.name == 'Maps-zu') {
+    	console.log(service);
+        mapboxAccessToken = service.credentials.authToken;
+    }
 });
 
-app.listen(port);
+app.get('/', function (req, res) {
+	app.locals.mapboxAccessToken
+	res.render('index', 
+		{'mapboxAccessToken' : mapboxAccessToken});
+});
+ 
+var server = app.listen(port, function () {
+  console.log('Example app started')
+});
 
-require("cf-deployment-tracker-client").track();
+
